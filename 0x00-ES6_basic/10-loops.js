b721d28f9a83f6883fab26021bf22e91fb304636
deking254 <dekingsky@gmail.com>
2023-11-27 18:50:41 +0000

    implemented some code on the 11and 12th file

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+  let i = 0;
+  for (let idx of array) {
+    let value = idx;
+    array[i] = appendString + value;
+    i++;
   }
-
   return array;
 }
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,13 @@
 export default function createReportObject(employeesList) {
+	return {
+		allEmployees: employeesList,
+		getNumberOfDepartments(employeesList){
+			let length = 0;
+			for (let i in employeesList){
+				length++;
+			}
+			return length;
+		}
 
+	};
 }
