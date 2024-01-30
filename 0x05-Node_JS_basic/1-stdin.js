99a4a1ea5a654b18e49bc09afa699d76d8dcb01a
deking254 <dekingsky@gmail.com>
2024-01-30 10:06:01 +0000

    Code: added code for the first two files

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+const readable = require('readable-stream')
+console.log('Welcome to Holberton School, what is your name?');
+process.on('exit', () => console.log('This important software is now closing'));
+process.stdin.on('data', handledata);
+process.openStdin();
+function handledata(data){
+	var stringdata = data.toString('utf8', 0, data.byteLength - 1);
+	console.log('Your name is: %s', stringdata)		
+}
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,1 @@
+This project looks at how to create a small http server using nodejs and javascript. It also looks at expressjs
