6eb232654550b4e55897f170f045b2c925079e2f
deking254 <dekingsky@gmail.com>
2024-01-31 07:29:09 +0000

    Style: fixing lint issues

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 #!/bin/node
 console.log('Welcome to Holberton School, what is your name?');
 process.on('exit', () => console.log('This important software is now closing'));
+function handledata(data) {
+  const stringdata = data.toString('utf8', 0, data.byteLength - 1);
+  console.log('Your name is: %s', stringdata);
+}
 process.stdin.on('data', handledata);
 process.openStdin();
-function handledata(data){
-	var stringdata = data.toString('utf8', 0, data.byteLength - 1);
-	console.log('Your name is: %s', stringdata)		
-}
