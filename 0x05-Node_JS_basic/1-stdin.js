d8f3204fdc7744c61f35c1f825c3a9ebda4b8573
deking254 <dekingsky@gmail.com>
2024-01-31 13:02:22 +0000

    TEST: testing with mocha

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-#!/bin/node
 console.log('Welcome to Holberton School, what is your name?');
-process.on('exit', () => console.log('This important software is now closing'));
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
 process.stdin.on('data', (data) => {
-  const stringdata = `Your name is: ${data.toString('utf8', 0, data.byteLength - 1)}`;
-  process.stdout.write(`${stringdata}\r`);
-  process.stdout.write('\n');
+  const stringdata = `Your name is: ${data.toString()}`;
+  process.stdout.write(`${stringdata}`);
 });
 process.openStdin();
