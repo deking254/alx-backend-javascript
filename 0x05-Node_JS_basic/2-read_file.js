55c53a150df2f89e33e1783fca6ca4584e4d43a3
deking254 <dekingsky@gmail.com>
2024-02-01 06:05:58 +0000

    TEST: fixes issues with mocha

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,36 +1,34 @@
 const file = require('fs');
 
 function countStudents(path) {
-  file.exists(path, (r) => {
-    if (r) {
-      file.readFile(path, (r, contents) => {
-        const content = contents.toString();
-        const array = content.split('\n');
-        const numberOfStudents = array.length - 2;
-        const countryPeople = {};
-        const countries = [];
-        array.forEach((line) => {
-          if (line.length > 0) {
-            const fields = line.split(',');
-            if (countryPeople[fields[3]]) {
-              countryPeople[fields[3]].push(fields[0]);
-            } else if (fields[3] !== 'field') {
-              countries.push(fields[3]);
-              countryPeople[fields[3]] = [];
-              countryPeople[fields[3]].push(fields[0]);
-            }
+  if (file.existsSync) {
+    try {
+      const content = file.readFileSync(path).toString();
+      const array = content.split('\n');
+      const numberOfStudents = array.length - 2;
+      const countryPeople = {};
+      const countries = [];
+      array.forEach((line) => {
+        if (line.length > 0) {
+          const fields = line.split(',');
+          if (countryPeople[fields[3]]) {
+            countryPeople[fields[3]].push(fields[0]);
+          } else if (fields[3] !== 'field') {
+            countries.push(fields[3]);
+            countryPeople[fields[3]] = [];
+            countryPeople[fields[3]].push(fields[0]);
           }
-        });
-        console.log('Number of students: %d', numberOfStudents);
-        countries.forEach((country) => {
-          const numberOfPeople = countryPeople[country].length;
-          const people = countryPeople[country].join(', ');
-          console.log('Number of students in %s: %d. List: %s', country, numberOfPeople, people);
-        });
+        }
       });
-    } else {
+      console.log(`Number of students: ${numberOfStudents}`);
+      countries.forEach((country) => {
+        const numberOfPeople = countryPeople[country].length;
+        const people = countryPeople[country].join(', ');
+        console.log(`Number of students in ${country}: ${numberOfPeople}. List: ${people}`);
+      });
+    } catch (error) {
       throw Error('Cannot load the database');
     }
-  });
+  }
 }
 module.exports = countStudents;
