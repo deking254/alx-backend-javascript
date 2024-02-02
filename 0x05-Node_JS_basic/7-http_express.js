55350156328f54ab8948d597b122cffb9ed545d5
deking254 <dekingsky@gmail.com>
2024-02-02 11:35:13 +0000

    Code: code for 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,1 +1,14 @@
-#!/bin/node
+const express = require('express');
+const databaseReader = require('./3-read_file_async');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'content-type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+app.get('/students', (req, res) => {
+  res.writeHead(200, { 'content-type': 'text/plain' });
+  databaseReader(process.argv[2]).then((a) => { res.end(a); });
+});
+app.listen(1245);
+module.exports = app;
