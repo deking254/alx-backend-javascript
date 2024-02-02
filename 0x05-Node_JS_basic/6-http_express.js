fed0502ff1feffcc661e58a8769cc7709d77aef5
deking254 <dekingsky@gmail.com>
2024-02-02 11:20:25 +0000

    Style: fixed eslint issues

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res)=>{
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
   res.end('Hello Holberton School!');
-})
-app.listen(1245)
+});
+app.listen(1245);
 module.exports = app;
