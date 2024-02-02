7803a829e463e7a08791b742b79869eda2e1c765
deking254 <dekingsky@gmail.com>
2024-02-02 07:10:28 +0000

    CODE: code for the 4-http.js file

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,1 +1,8 @@
-#!/bin/node
+const server = require('http');
+
+const app = server.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+app.listen(1245, '0.0.0.0', () => {});
+module.exports = app;
---
+++ 0x05-Node_JS_basic/4_main.js
@@ -0,0 +1,2 @@
+a = require('./4-http.js')
+console.log(a._events.request);
