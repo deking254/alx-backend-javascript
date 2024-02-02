ff5d9c5e9726246b4896fdc3a7c8444c037adc86
deking254 <dekingsky@gmail.com>
2024-02-02 06:04:08 +0000

    Style: fixing eslint

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,41 +1,42 @@
 const file = require('fs');
 
 function countStudents(path) {
-      try{
-      a =  new Promise((resolve, reject)=>{
+  try {
+    const a = new Promise((resolve) => {
       file.readFile(path, (r, contents) => {
-       if (r === null){
-	const content = contents.toString();
-        const array = content.split('\n');
-        const numberOfStudents = array.length - 2;
-        const countryPeople = {};
-        const countries = [];
-        array.forEach((line) => {
-          if (line.length > 0) {
-            const fields = line.split(',');
-            if (countryPeople[fields[3]]) {
-              countryPeople[fields[3]].push(fields[0]);
-            } else if (fields[3] !== 'field') {
-              countries.push(fields[3]);
-              countryPeople[fields[3]] = [];
-              countryPeople[fields[3]].push(fields[0]);
+        if (r === null) {
+          const content = contents.toString();
+          const array = content.split('\n');
+          const numberOfStudents = array.length - 2;
+          const countryPeople = {};
+          const countries = [];
+          array.forEach((line) => {
+            if (line.length > 0) {
+              const fields = line.split(',');
+              if (countryPeople[fields[3]]) {
+                countryPeople[fields[3]].push(fields[0]);
+              } else if (fields[3] !== 'field') {
+                countries.push(fields[3]);
+                countryPeople[fields[3]] = [];
+                countryPeople[fields[3]].push(fields[0]);
+              }
+            }
+          });
+          console.log(`Number of students: ${numberOfStudents}`);
+          for (let i = 0; i < countries.length; i += 1) {
+            const numberOfPeople = countryPeople[countries[i]].length;
+            const people = countryPeople[countries[i]].join(', ');
+            console.log(`Number of students in ${countries[i]}: ${numberOfPeople}. List: ${people}`);
+            if (i === countries.length - 1) {
+              resolve();
             }
           }
-        });
-        console.log(`Number of students: ${numberOfStudents}`);
-        for(i=0; i < countries.length; i++){
-          let numberOfPeople = countryPeople[countries[i]].length;
-          let people = countryPeople[countries[i]].join(', ');
-          console.log(`Number of students in ${countries[i]}: ${numberOfPeople}. List: ${people}`);
-	  if (i === countries.length - 1){
-		  resolve();
-	  }
-        };
-	
-      }})
-})}catch(e){
-	throw Error('Cannot load the database');
+        }
+      });
+    });
+    return a;
+  } catch (e) {
+    throw Error('Cannot load the database');
+  }
 }
-	return a;
-  };
 module.exports = countStudents;
