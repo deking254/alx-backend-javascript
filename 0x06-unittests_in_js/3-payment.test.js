73a42b119c47cba2bbb01e74dddf72b93d61640e
deking254 <dekingsky@gmail.com>
2024-02-04 12:35:30 +0000

    Code: code for 3-payment.test.js

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,12 @@
+const { spy } = require('sinon');
+const { expect } = require('chai');
+const Util = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
+let a = spy(Util, 'calculateNumber');
+describe('sendPaymentRequestToApi', ()=>{
+  it('testing', (done)=>{
+    sendPaymentRequestToApi(100, 20)
+    expect(a.calledWith('SUM', 100, 20)).to.be.true;
+    done();
+  })
+})
