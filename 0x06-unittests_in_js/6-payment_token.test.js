8ea5ca80708bc60875a4b42f4b54d70be3c3fbf6
deking254 <dekingsky@gmail.com>
2024-02-05 05:50:52 +0000

    Code: code for 6-payment_token.test.js

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token.js');
+describe('getPaymentTokenFromAPI', ()=>{
+  it('testing', (done)=>{
+    getPaymentTokenFromAPI(true).then((a)=>{
+      try{
+        expect(a).to.equal({data: 'Successful response from the API' });
+        done();
+      } catch (e) {
+        done();
+      }
+    })
+  })
+})
