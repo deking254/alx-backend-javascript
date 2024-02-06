01b7adaf9827d7f8c44faf172b64ca142ca9f752
deking254 <dekingsky@gmail.com>
2024-02-06 07:30:40 +0000

    Code: added the tests for the route path /

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -4,6 +4,18 @@ const request = require('request');
 let spyy = spy(console, 'log');
 describe('index_page', ()=>{
     it('test_status_code', (done)=>{
+      request.get('http://localhost:7865', (error, response)=>{
+        expect(response.statusCode).to.be.equal(200);
+        done()
+      });
+    })
+    it('test_message', (done)=>{
+      request.get('http://localhost:7865', (error, response)=>{
+        expect(response.body).to.be.equal('Welcome to the payment system');
+        done()
+      });
+    })
+    it('test_status_code', (done)=>{
       request.get('http://localhost:7865/cart/14', (error, response)=>{
         expect(response.statusCode).to.be.equal(200);
 	done()
